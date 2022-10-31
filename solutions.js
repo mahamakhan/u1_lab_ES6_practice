85ef6f85ed869765db24cd228417bf80ed861cce
mahamakhan <mahamimranamjad@gmail.com>
2022-10-31 14:52:10 -0700

    l

--- solutions.js
+++ solutions.js
@@ -1,1 +1,36 @@
+const even = [2, 4, 6, 8]
+const odd = [1, 3, 5, 7, 9]
 
+const allNumbers = [...even, ...odd]
+
+// console.log(allNumbers)
+
+const faveFoods = {
+  nihari: Pakistan,
+  pizza: italy,
+  pasta: america
+}
+
+const janetFaveFoods = {
+  burger: UK,
+  burrito: Mexico,
+  thai: Thailand
+}
+
+const allFoods = { ...faveFoods, ...janetFaveFoods }
+console.log(allFoods)
+
+const myself = {
+  name: Maham,
+  age: 26,
+  hometown: Lahore
+}
+
+console.log(name)
+console.log(age)
+console.log(hometown)
+
+const pet = { name: Zelda, age: 4, breed: Lab }
+
+const me = [pet, myself]
+console.log(me)
